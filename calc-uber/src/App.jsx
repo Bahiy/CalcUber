0def2ae02e75c5ceafe9f12596b1989ebe9ac7b9
Bahiy <bahiy14@gmail.com>
2024-06-14 01:17:09 +0100

    Add Styles in button and inputs. Fix the problem in inputs with ','

--- calc-uber/src/App.jsx
+++ calc-uber/src/App.jsx
@@ -1,16 +1,14 @@
-import "./App.css"
+import "./App.css";
 import { useState } from "react";
 
-
 const App = () => {
-
   // Estado inicial como um objeto com chaves para cada input
   const [formValues, setFormValues] = useState({
-    valorGasolina: '',
-    kmPorLitro: '',
-    kmRodados: '',
-    totalDoDia: '',
-    gastoPorc: 0
+    valorGasolina: "",
+    kmPorLitro: "",
+    kmRodados: "",
+    totalDoDia: "",
+    gastoPorc: 0,
   });
 
   const [isVisible, setIsVisible] = useState(false);
@@ -20,46 +18,48 @@ const App = () => {
     const { name, value } = event.target;
     setFormValues({
       ...formValues,
-      [name]: value
+      [name]: value,
     });
   };
 
-
   // Função para capturar os valores dos inputs quando o botão "Calcular" for pressionado
   const handleSubmit = () => {
-    const valorGasolina = parseFloat(formValues.valorGasolina);
+    const valorGasolinastr = formValues.valorGasolina.replace(",", ".");
+    const valorGasolina = parseFloat(valorGasolinastr)
     const kmPorLitro = parseFloat(formValues.kmPorLitro);
     const kmRodados = parseFloat(formValues.kmRodados);
     const totalDoDia = parseFloat(formValues.totalDoDia);
 
-    if (isNaN(valorGasolina) || isNaN(kmPorLitro) || isNaN(kmRodados) || isNaN(totalDoDia)) {
-      alert('Por favor, insira valores numéricos válidos.');
-      return;
+    if (
+      isNaN(valorGasolina) ||
+      isNaN(kmPorLitro) ||
+      isNaN(kmRodados) ||
+      isNaN(totalDoDia)
+    ) {
+      return alert("Por favor, insira valores numéricos válidos.");
+
     }
 
-    const custoKM = formValues.valorGasolina / formValues.kmPorLitro;
+    const custoKM = valorGasolina / kmPorLitro;
     console.log(`O custo por KM é: ${custoKM}`);
-    const custoDia = formValues.kmRodados * custoKM;
-    const gastoPorc = (custoDia * 100) / formValues.totalDoDia;
+    const custoDia = kmRodados * custoKM;
+    const gastoPorc = (custoDia * 100) / totalDoDia;
 
     console.log(`Quantos % o gasto representa: ${gastoPorc}%`);
+    console.log(typeof(valorGasolina));
 
     // Atualiza o estado usando setFormValues
     setFormValues((prevState) => ({
       ...prevState,
-      gastoPorc: parseFloat(gastoPorc.toFixed(2))
+      gastoPorc: parseFloat(gastoPorc.toFixed(2)),
     }));
 
     // Torna a seção de resultado visível após o cálculo
     setIsVisible(true);
-  }
-
-
-
+  };
 
   return (
     <div className="App">
-
       <div>
         <h1>Calculadora UBER</h1>
         <p>Saiba o quanto o seu dia gerou de lucro</p>
@@ -99,28 +99,29 @@ const App = () => {
         />
 
         <br />
-        <button type="button" onClick={handleSubmit}>Calcular</button>
+        <button type="button" onClick={handleSubmit}>
+          Calcular
+        </button>
       </div>
 
       {isVisible && (
-
         <div className="resultado">
           <h2>Resultado:</h2>
           <div>
-            {
-              formValues.gastoPorc <= 26
-                ? <div>Excelente</div>
-                : formValues.gastoPorc <= 31
-                  ? <div>Fique em alerta</div>
-                  : <div>Você vai quebrar</div>
-            }
+            {formValues.gastoPorc <= 26 ? (
+              <div>Excelente</div>
+            ) : formValues.gastoPorc <= 31 ? (
+              <div>Fique em alerta</div>
+            ) : (
+              <div>Você vai quebrar</div>
+            )}
 
             <p>O gasto representa {formValues.gastoPorc}% da sua renda.</p>
           </div>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
