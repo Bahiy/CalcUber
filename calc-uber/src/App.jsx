ea2992775f5e76a2ef5a3cb54f264b0f580bc3a3
Bahiy <bahiy14@gmail.com>
2024-07-30 03:18:46 -0300

    Feature: added net yield

--- calc-uber/src/App.jsx
+++ calc-uber/src/App.jsx
@@ -9,6 +9,12 @@ const App = () => {
     kmRodados: null,
     totalDoDia: null,
     gastoPorc: null,
+    gastoValor: (a, b) => {
+      const c = a * b;
+      const prod = c / 100;
+      return prod;
+    },
+    valorLiquido: null,
   });
 
   const [isVisible, setIsVisible] = useState(false);
@@ -19,13 +25,12 @@ const App = () => {
     setFormValues({
       ...formValues,
       [name]: value,
-
     });
   };
 
   // Função para capturar os valores dos inputs quando o botão "Calcular" for pressionado
   const handleSubmit = () => {
-    const valorGasolina = parseFloat(formValues.valorGasolina)
+    const valorGasolina = parseFloat(formValues.valorGasolina);
     const kmPorLitro = parseFloat(formValues.kmPorLitro);
     const kmRodados = parseFloat(formValues.kmRodados);
     const totalDoDia = parseFloat(formValues.totalDoDia);
@@ -42,7 +47,9 @@ const App = () => {
     const custoKM = valorGasolina / kmPorLitro;
     console.log(`O custo por KM é: ${custoKM}`);
     const custoDia = kmRodados * custoKM;
+    console.log(custoDia);
     const gastoPorc = (custoDia * 100) / totalDoDia;
+    const valorRestante = totalDoDia - custoDia;
 
     console.log(`Quantos % o gasto representa: ${gastoPorc}%`);
 
@@ -50,6 +57,7 @@ const App = () => {
     setFormValues((prevState) => ({
       ...prevState,
       gastoPorc: parseFloat(gastoPorc.toFixed(2)),
+      valorLiquido: parseFloat(valorRestante.toFixed(2)),
     }));
 
     // Torna a seção de resultado visível após o cálculo
@@ -90,7 +98,7 @@ const App = () => {
 
         <br />
         <button
-          className="rounded-full border-solid border-2 border-black p-2 m-2 bg-cyan-700"
+          className="rounded-full border-solid text-xl border-2 border-black px-5 py-2 m-3 bg-cyan-600 hover:bg-cyan-900"
           type="button"
           onClick={handleSubmit}
         >
@@ -99,19 +107,50 @@ const App = () => {
       </div>
 
       {isVisible && (
-        <div className="resultado">
-          <div>
+        <div className="flex flex-row content-center items-center justify-center text-center">
+
+          <div className="bg-zinc-950 border-solid rounded-2xl border-2 border-gray-400 m-2 text-center p-5 w-64 h-36">
             {formValues.gastoPorc <= 26 ? (
-              <p className="text-green-500 font-semibold text-3xl">Excelente!</p>
+              <span className="text-green-500 font-semibold text-3xl">
+                Excelente!
+              </span>
             ) : formValues.gastoPorc <= 31 ? (
-              <p className="text-yellow-500 font-semibold text-3xl">
-                Fique em alerta!
-              </p>
+              <span className="text-yellow-500 font-semibold text-3xl">
+                Alerta!
+              </span>
             ) : (
-              <p className="text-red-500 font-semibold text-3xl">Péssimo!</p>
+              <span className="text-red-500 font-semibold text-3xl">
+                Péssimo!
+              </span>
             )}
 
-            <p>O gasto representa <strong>{formValues.gastoPorc}</strong>% da sua renda.</p>
+            <p className="text-sm mt-2">
+              {formValues.gastoPorc}% representa R$
+              {formValues
+                .gastoValor(formValues.totalDoDia, formValues.gastoPorc)
+                .toFixed(2)}{" "}
+              do seu Faturamento <b>Bruto</b>
+            </p>
+          </div>
+
+          <div className="bg-zinc-950 border-solid rounded-2xl border-2 border-gray-400 m-2 text-center content-center p-5 w-64 h-36">
+            {formValues.gastoPorc <= 26 ? (
+              <span className="text-green-500 font-semibold text-3xl">
+                R$ {formValues.valorLiquido}
+              </span>
+            ) : formValues.gastoPorc <= 31 ? (
+              <span className="text-yellow-500 font-semibold text-3xl">
+                R$ {formValues.valorLiquido}
+              </span>
+            ) : (
+              <span className="text-red-500 font-semibold text-3xl">
+                R$ {formValues.valorLiquido}
+              </span>
+            )}
+
+            <p>
+              <b>Rendimento Liquido</b>
+            </p>
           </div>
         </div>
       )}
